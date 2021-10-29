83f4ac1516c562274b6dda05f311d592f485219c
RamsesCamas <machiniram@gmail.com>
2021-10-29 00:21:55 -0500

    Finished project

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 import regex
 from random import randint
+import urllib.request
+from PIL import Image, ImageTk
+from tkinter import Tk, Label, Button, font
+from functools import partial
+import os
 
-"""
-TODO: Implementar UI en Tkiner
-"""
 
 URL_MOVIES = 'https://www.imdb.com/chart/top/'
 URL_TV_SHOWS = 'https://www.imdb.com/chart/toptv/'
@@ -12,7 +14,7 @@ URL_TV_SHOWS = 'https://www.imdb.com/chart/toptv/'
 
 REGEX_ALL_TITLE = r'<td class="titleColumn">[\s\n]*.*[\s\n]*<a href=".*"[\s\n]title=".*"\s*>.*</a>[\s\n]*<span class="secondaryInfo">.*</span>[\s\n]*</td>'
 
-REGEX_TITLE = r'(?<=<td class="titleColumn">[\s\n]*[0-9]{1,3}\.[\s\n]*<a href="[\w/]+"\ntitle="[\w\u00C0-\u017F\s\\,\(\.\)]+"\s*>)(.*)(?=</a>)'
+REGEX_TITLE = r'(?<=<td class="titleColumn">[\s\n]*[0-9]{1,3}\.[\s\n]*<a href="[\w/]+"\ntitle="[\w\u00C0-\u017F\s\\,\(\.\)\-]+"\s*>)(.*)(?=</a>)'
 
 REGEX_YEAR = r'(?<=<span class="secondaryInfo">)(.*)(?=</span>)'
 
@@ -31,28 +33,61 @@ def show_movie(title_matches,rating_matches,poster_matches):
     movie_year = regex.search(REGEX_YEAR,movie_match).group(0)
     try:
         movie_director = regex.search(REGEX_MOVIE_DIRECTOR,movie_match).group(0)
-        movie_director = f'\nDirector: {movie_director}'
+        movie_director = f'Director: {movie_director}'
         movie_main_cast = regex.search(REGEX_MOVIE_MAIN_CAST,movie_match).group(0)
-        movie_main_cast = f'Cast: {movie_main_cast}\n'
+        movie_main_cast = f'Cast: {movie_main_cast}'
     except:
         movie_director = ''
         movie_main_cast = ''
     movie_rating = rating_matches[num_movie]
     movie_poster = poster_matches[num_movie]
-    print(f'{movie_title} - Año: {movie_year} - {movie_director}'+
-    f'{movie_main_cast}Puntuación: {movie_rating}')
-    print(movie_poster)
+    label_title_year['text'] = movie_title + '\n' + 'Año: ' + movie_year
+    label_extra_info['text'] = movie_director + '\n' + movie_main_cast
+    label_rating['text'] = f'Puntuación: {movie_rating}'
 
-if __name__ == '__main__':
-    recommendation = input('Peliculas (P) o Series de TV (S): ')
-    if recommendation == 'P':
+    urllib.request.urlretrieve(movie_poster,"poster.jpg")
+    image_movie = Image.open('poster.jpg')
+    image_movie = image_movie.resize((300,500))
+    new_image = ImageTk.PhotoImage(image_movie)
+    label_image = Label(root,image=new_image)
+    label_image.image = new_image
+    label_image.place(x=900,y=100)
+    os.remove('poster.jpg')
+
+def main(choice):
+    if choice == 'P':
         response = requests.get(URL_MOVIES)
-        print('Peliculas recomendadas:\n')
-    elif recommendation == 'S':
+        label1['text'] = 'Pelicula recomendada:'
+    elif choice == 'TV':
         response = requests.get(URL_TV_SHOWS)
-        print('Series de TV recomendadas:\n')
+        label1['text'] = 'Serie de TV recomendada:'
     if response.status_code == 200:
         title_matches = regex.findall(REGEX_ALL_TITLE,response.text)
         rating_matches = regex.findall(REGEX_RATING,response.text)
         poster_matches = regex.findall(REGEX_POSTER,response.text)
         show_movie(title_matches,rating_matches,poster_matches)
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Sistema de Recomendaciones')
+    root.geometry('1280x720')
+    
+    btn_movie = Button(root,text='Recomendar película',height=1,command=partial(main,'P'),font=font.Font(size=13))
+    btn_movie.place(x=0,y=100)
+
+    btn_tv_show = Button(root,text='Recomendar serie de TV',height=1,command=partial(main,'TV'),font=font.Font(size=13))
+    btn_tv_show.place(x=0,y=300)
+
+    label1 = Label(root,height=1,width=30,font=font.Font(size=14))
+    label1.place(x=300,y=50)
+
+    label_title_year = Label(root,height=2,width=35,text='',font=font.Font(size=13))
+    label_title_year.place(x=400,y=200)
+
+    label_extra_info = Label(root,height=2,width=35,text='',font=font.Font(size=13))
+    label_extra_info.place(x=400,y=350)
+
+    label_rating = Label(root,height=2,width=35,text='',font=font.Font(size=13))
+    label_rating.place(x=400,y=500)
+
+    root.mainloop()
